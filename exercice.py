f0647dd7a39c5dc635669283be8666df2b4fd202
ArXives <imane.znadi@polymtl.ca>
2020-10-19 14:29:39 -0400

    exercice

--- exercice.py
+++ exercice.py
@@ -13,21 +13,54 @@ def build_note_dictionaries(note_names, add_octave_no=True):
 	midi_to_name = {}
 	name_to_midi = {}
 	# Pour chaque octave de 0 à 8 (inclus). On va générer tout l'octave 8, même si la dernière note du piano est Do 8
+	for octave in range(8+1):
 		# Pour chaque note de l'octave
+		for note in range(NOTES_PER_OCTAVE):
 			# Calculer le numéro MIDI de la note et ajouter aux deux dictionnaires
+			midi_no = C0_MIDI_NO +octave +NOTES_PER_OCTAVE + note
 			# Ajouter le numéro de l'octave au nom de la note si add_octave_no est vrai
+			full_note_name = note_names[note] + (str(octave) if add_octave_no else "")
 			# Garder les numéros de notes dans name_to_midi entre 0 et 11 si add_octave_no est faux
+			name_to_midi[full_note_name] = midi_no if add_octave_no else midi_no % NOTES_PER_OCTAVE
 	return midi_to_name, name_to_midi
 
 def build_print_note_name_callback(midi_to_name):
-	pass
+	# fonction locale
+	def callback(midi_msg):
+		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
+			print(midi_to_name[midi_msg.note])
+	return callback
 
 def build_print_chord_name_callback(chord_names_and_notes, name_to_midi):
 	# Construire le dictionnaire d'assocations entre état des notes et accord joué.
+	chords = {}
+	for name, notes in chord_names_and_notes.items():
+		chord_notes = [False] * 12
+		for note in notes:
+			name_to_midi[note]
+			chord_notes[name_to_midi[note] % NOTES_PER_OCTAVE] = True
+		chords[tuple(chord_notes)] = name
+
+	c_major = chords[(1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0)]
 	
 	# Créez et retourner le callback
-	pass
+	def callback(midi_msg):
+		global notes_states
+		# si une note est appuyée
+		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
+			#Je mets son élément correspondant dans l'état du clavier à True
+			notes_states[midi_msg.note % NOTES_PER_OCTAVE] = True
+			notes_states_tuple = tuple(notes_states)
+			# si accord connu
+			if notes_states_tuple in chords:
+				print(chords[notes_states_tuple])
+		# si non si une note est relaché
+		elif midi_msg.type == "note_off" or (midi_msg.type == "note_on" and midi_msg.velocity == 0):
+			#Idem mais à False
+			notes_states[midi_msg.note % NOTES_PER_OCTAVE] = False
+	return callback
 
+notes_states = [False] * 12
 
 def main():
 	PORT_MIDI = "UnPortMIDI 0"
